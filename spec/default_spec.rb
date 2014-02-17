545f4a27e88ad1850251094e67aa6bc05d644401
M Parker <mparker17@536298.no-reply.drupal.org>
2014-02-17 18:56:33 -0500

    Added chefspec test for default recipe.

---
+++ spec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'chefspec'
+
+describe 'chef-drupal::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it 'logs noop' do
+    expect(chef_run).to write_log('Noop.')
+  end
+end
