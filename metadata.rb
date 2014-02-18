2ae2eeb11010be5ed56cbd3624c40a91c309396e
M Parker <mparker17@536298.no-reply.drupal.org>
2014-02-17 19:10:02 -0500

    Added PHP, MySQL as recommendations for this cookbook.
    This cookbook mainly does stuff on the filesystem, and the fileserver
    may not necessarily be the app or database server (i.e.: PHP and MySQL
    are not requirements).

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'GPL v2'
 description      'Sets up Drupal core'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
+
+recommends 'php'
+recommends 'mysql'
